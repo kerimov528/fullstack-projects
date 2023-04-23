9dbff8d23300621a50ccb6d9432daef69708a23c
kerimov528 <kerimov.anar528@gmail.com>
2023-04-23 13:12:37 -0700

    Server and Client First Configuration Ready

---
+++ admin-dashboard/server/index.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import morgan from 'morgan'
+import mongoose from 'mongoose'
+import helmet from 'helmet'
+import dotenv from 'dotenv'
+import clientRoutes from './routes/client.js'
+import generalRoutes from './routes/general.js'
+import managementRoutes from './routes/management.js'
+import salesRoutes from './routes/sales.js'
+
+/* Configuration */
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+app.use(morgan('common'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+
+/* Routes */
+
+app.use('/client', clientRoutes)
+app.use('/general', generalRoutes)
+app.use('/management', managementRoutes)
+app.use('/sales', salesRoutes)
+
+
+/* Mongoose Setup */
+
+const PORT = process.env.PORT || 9000
+mongoose.connect(process.env.MONGO__URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log('Server listen...'))
+}).catch((err) => console.log(`${err} didn't connect...`))
